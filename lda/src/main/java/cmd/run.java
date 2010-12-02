afdf56a9a97ea02537d8d98c83dda93eca8a7949
Chris Dollin <ehog.hedge@gmail.com>
2010-12-02 14:51:00 +0000

    add -D option processing to run command

--- lda/src/main/java/cmd/run.java
+++ lda/src/main/java/cmd/run.java
@@ -6,22 +6,32 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class run 
 	{
-	public static void main( String [] ignored ) throws IOException, InterruptedException 
+	public static void main( String [] args ) throws IOException, InterruptedException 
 		{
 		String toDir = "Elda_1.0";
 		unzipJarfile( toDir, "elda.jar" );
 	//
-		ProcessBuilder pb = new ProcessBuilder( "java", "-jar", "start.jar" );
+		ProcessBuilder pb = new ProcessBuilder( buildArgs( args ) );
 		pb.directory( new File( toDir ) );
 		pb.redirectErrorStream( true );
-		Process p = pb.start();
 	//
+		forwardOutput( pb.start() );
+		}
+
+	/**
+	    copy whatever the process <code>p</code> produces to standard
+	    output. Give up on any exception.
+	*/
+	private static void forwardOutput( Process p ) throws IOException 
+		{
 		byte [] bytes = new byte[1024];
 		BufferedInputStream is = new BufferedInputStream( p.getInputStream() );
 		while (true) 
@@ -33,6 +43,30 @@ public class run
 			}
 		}
 
+	/**
+	    Build the args for the Jetty command line that will be launched
+	    when we've unzipped the jarfile. Any of <code>args</code> that
+	    start with -D will be inserted after the java command; any that
+	    don't will be inserted after start.jar.
+	*/
+	private static List<String> buildArgs( String[] args ) 
+		{
+		List<String> pargs = new ArrayList<String>();
+		List<String> postponed = new ArrayList<String>();
+		pargs.add( "java" );
+		for (String arg: args) 
+			(arg.startsWith("-D") ? pargs : postponed).add( arg );
+		pargs.add( "-jar" );
+		pargs.add( "start.jar" );
+		pargs.addAll( postponed );
+		return pargs;
+		}
+
+	/**
+	    Unzip the file named <code>jarFile</code> into the directory named
+	    <code>toDir</code>. Give up if there are any exceptions, which may
+	    trash existing files and leave incomplete new ones.
+	*/
 	public static void unzipJarfile( String toDir, String jarFile ) throws IOException 
 		{
 		byte [] buffer = new byte[100 * 1024];
