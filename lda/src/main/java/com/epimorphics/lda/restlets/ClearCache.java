39f1fc00abab176f247694389803cd631f813a2c
Chris Dollin <ehog.hedge@gmail.com>
2011-01-17 12:10:47 +0000

    Fixed copyright dates to 2011.

--- lda/src/main/java/com/epimorphics/lda/restlets/ClearCache.java
+++ lda/src/main/java/com/epimorphics/lda/restlets/ClearCache.java
@@ -1,3 +1,11 @@
+/*
+    See lda-top/LICENCE (or http://elda.googlecode.com/hg/LICENCE)
+    for the licence for this software.
+    
+    (c) Copyright 2011 Epimorphics Limited
+    $Id$
+*/
+
 package com.epimorphics.lda.restlets;
 
 import javax.ws.rs.GET;
