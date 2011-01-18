b9b12a109b10c35a817a9b20d223d0b7102f5264
Chris Dollin <ehog.hedge@gmail.com>
2011-01-18 14:41:11 +0000

    Updates.

--- deliver-elda/deliver.bash
+++ deliver-elda/deliver.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export VERSION=0.9.8
 #
 # script to construct a delivery of elda. this should really
 # be some cool maven invocation(s) but I don't know which
@@ -17,9 +18,10 @@ rm -rf delivery
 # use maven to generate the webapp -- this is mostly
 # just getting the right jars in the right place.
 
-(cd ../json-rdf; mvn compile package install)
-(cd ../lda; mvn compile package install)
-mvn package
+(cd ../json-rdf; mvn -Delda.version=$VERSION compile package install)
+(cd ../lda;      mvn -Delda.version=$VERSION compile package install)
+(cd ..;          mvn -Delda.version=$VERSION compile package install)
+mvn -Delda.version=$VERSION package
 
 #
 # clone the built-in jetty distribution into 'delivery'.
@@ -45,14 +47,14 @@ cp -r src/main/docs delivery/webapps/elda
 # extract the unzip-and-run command class from the lda jar
 #
 
-(cd delivery; unzip $HERE/target/elda/WEB-INF/lib/lda-0.1.0-SNAPSHOT.jar cmd/run.class)
+(cd delivery; unzip $HERE/target/elda/WEB-INF/lib/lda-$VERSION.jar cmd/run.class)
 
 #
 # make the delivery area into a executable jar that runs cmd.run,
 # and leave that jar in our current directory.
 #
 
-(cd delivery; jar cfe $HERE/elda.jar cmd.run .)
+(cd delivery; jar cfe $HERE/elda-$VERSION.jar cmd.run .)
 
 #
 # done
