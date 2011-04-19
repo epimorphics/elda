ac8d305d40493242b931acf099f89540d58fdd8a
Chris Dollin <ehog.hedge@gmail.com>
2011-04-19 11:54:59 +0100

    adding RE matcher; case 2: multiple non-patterned strings.

--- lda/src/test/java/com/epimorphics/lda/scratch/tests/Scratch_URI_Templates.java
+++ lda/src/test/java/com/epimorphics/lda/scratch/tests/Scratch_URI_Templates.java
@@ -13,8 +13,11 @@ import static org.junit.Assert.*;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -46,6 +49,26 @@ public class Scratch_URI_Templates {
 	}
 	
 	@Test public void search_res() {
+		ReBox<String> rb = new ReBox<String>();
+		rb.add( "/foo/bar/baz", "A" );
+		assertEquals( "A", rb.match( "/foo/bar/baz" ) );
+		assertEquals( null, rb.match( "/foo/bar/ba" ) );
+		assertEquals( null, rb.match( "/foo/bar/bazz" ) );
+	}
+	
+	@Test public void search_res2() {
+		ReBox<String> rb = new ReBox<String>();
+		rb.add( "/foo/bar/baz", "A" );
+		rb.add( "/foo/bar", "B" );
+		rb.add( "/foo/bar/baz/bert", "C" );
+		assertEquals( "A", rb.match( "/foo/bar/baz" ) );
+		assertEquals( "B", rb.match( "/foo/bar" ) );
+		assertEquals( "C", rb.match( "/foo/bar/baz/bert" ) );
+		assertEquals( null, rb.match("/foo/bar/b") );
+		assertEquals( null, rb.match("/foo/bar/baz/berti") );
+	}
+	
+	@Test public void search_res3() {
 		ReBox<String> rb = new ReBox<String>();
 		rb.add( "/foo/bar/baz", "A" );
 		assertEquals( "A", rb.match( "/foo/bar/baz" ) );
@@ -92,7 +115,7 @@ public class Scratch_URI_Templates {
 		
 		int counter = 0;
 		
-		Map<Integer, Integer> states = new HashMap<Integer, Integer>();
+		Map<Integer, Set<Integer>> states = new HashMap<Integer, Set<Integer>>();
 		Map<Integer, T> answers = new HashMap<Integer, T>();
 		
 		public int freshState() {
@@ -100,31 +123,58 @@ public class Scratch_URI_Templates {
 		}
 
 		public void go(int state, char ch, int next) {
-			states.put( (state << 16) + ch, next );
+			int key = (state << 16) + ch;
+			Set<Integer> to = states.get(key);
+			if (to == null) states.put(key, to = new HashSet<Integer>());
+			to.add(next);
 		}
 		
 		public void isfinal(int state, T value) {
 			answers.put(state, value);
 		}
 
+		private void showme() {
+			System.out.println( "Some State Machine" );
+			List<Map.Entry<Integer, Set<Integer>>> x = new ArrayList<Map.Entry<Integer,Set<Integer>>>(states.size());
+			for (Map.Entry<Integer, Set<Integer>> e: states.entrySet())	x.add( e );
+			Collections.sort( x, new Comparator<Map.Entry<Integer, Set<Integer>>>() {
+				@Override public int compare(Map.Entry<Integer, Set<Integer>> o1, Map.Entry<Integer, Set<Integer>> o2) {
+					return o1.getKey() - o2.getKey();
+				}})
+				;
+			for (Map.Entry<Integer, Set<Integer>> e: x) {
+				int sc = e.getKey();
+				System.out.println( " S" + (sc >> 16) + " " + (char)(sc & 0xff) + " => S*" + e.getValue() );
+			}
+			System.out.println( answers );
+		}
+		
 		public T match(String s) {
+//			showme();
 			Set<Integer> current = new HashSet<Integer>(); 
 			current.add(0);
 			Set<Integer> nexties = new HashSet<Integer>();
 		//
 			for (int i = 0; i < s.length(); i += 1) {
 				char ch = s.charAt(i);
+//				System.err.println( ">> doing: " + ch + ", current = " + current );
 				for (Integer state: current) {
-					Integer next = states.get( (state << 16) + ch );
-					if (next != null) {
-						nexties.add( next );
-					}
-				if (nexties.isEmpty()) return null;
-				Set<Integer> x = nexties; nexties = current; current = x; nexties.clear();
+					Set<Integer> next = states.get( (state << 16) + ch );
+					if (next != null) nexties.addAll( next );
 				}
+//				System.err.println( ">> nexties: " + nexties );
+				// if (nexties.isEmpty()) { System.err.println( ">> BOO!" ); return null; }
+				// Set<Integer> x = nexties; nexties = current; current = x; nexties.clear();
+				current = nexties; nexties = new HashSet<Integer>();
 			}
 		//
-			return answers.get( current.iterator().next() );
+			for (Integer state: current) {
+				T answer = answers.get(state);
+				if (answer != null) return answer;
+			}
+			return null;
+//			Iterator<Integer> it = current.iterator();
+//			return it.hasNext() ? answers.get( it.next() ) : null;
 		}
 		
 	}
