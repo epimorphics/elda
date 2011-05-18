b7a35a98d43d0c194f3a3565ead060a8a9d5583b
Chris Dollin <ehog.hedge@gmail.com>
2011-05-18 11:50:59 +0100

    releasing 1.1.4

--- lda/src/test/java/com/epimorphics/lda/scratch/tests/Scratch_URI_Templates.java
+++ lda/src/test/java/com/epimorphics/lda/scratch/tests/Scratch_URI_Templates.java
@@ -11,16 +11,7 @@ package com.epimorphics.lda.scratch.tests;
 import static org.junit.Assert.*;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import org.junit.Test;
 
