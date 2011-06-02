f98cf9718b005ae87012b95a52597ba60ecde74f
bwm <devnull@localhost>
2011-06-02 18:19:47 +0100

    Generate a source file with the maven version in it.

---
+++ lda/src/main/java/com/epimorphics/lda/Version.java
@@ -0,0 +1,5 @@
+package com.epimorphics.lda;
+
+public interface Version {
+	public final String string = "1.1.4d";
+}
---
+++ lda/src/pre/Version.java
@@ -0,0 +1,13 @@
+package com.epimorphics.lda;
+
+/**
+ * An interface only to hold the project version number.
+ * 
+ * <p>This source file is generated automatically by the build process.</p>
+ * 
+ * @author bwm
+ *
+ */
+public interface Version {
+	public final String string = "${project.version}";
+}
