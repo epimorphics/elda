f4bbf682fef99a16ec817c4053d5cc08fe254d4c
Chris Dollin <ehog.hedge@gmail.com>
2011-07-11 08:49:20 +0100

    to-merge-changes

--- lda/src/test/java/com/epimorphics/sources/test/QueryExecutionWithHistory.java
+++ lda/src/test/java/com/epimorphics/sources/test/QueryExecutionWithHistory.java
@@ -9,6 +9,7 @@
 package com.epimorphics.sources.test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -83,5 +84,24 @@ public class QueryExecutionWithHistory implements QueryExecution {
 
 	@Override public void abort() {
 		qe.abort();
+	}
+
+	@Override public void setTimeout(long arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override public void setTimeout(long arg0, TimeUnit arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override public void setTimeout(long arg0, long arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override public void setTimeout(long arg0, TimeUnit arg1, long arg2, TimeUnit arg3) {
+		
 	}
 }
