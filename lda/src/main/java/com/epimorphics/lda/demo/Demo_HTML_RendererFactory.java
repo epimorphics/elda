caf449cd6b87d83c074a727e2a85e6114417c7b1
Chris Dollin <ehog.hedge@gmail.com>
2011-11-25 14:39:35 +0000

    Removed auto-TODOs from renderer factory.

--- lda/src/main/java/com/epimorphics/lda/demo/Demo_HTML_RendererFactory.java
+++ lda/src/main/java/com/epimorphics/lda/demo/Demo_HTML_RendererFactory.java
@@ -17,17 +17,14 @@ import com.hp.hpl.jena.rdf.model.Resource;
 public class Demo_HTML_RendererFactory implements RendererFactory {
 
 	@Override public Renderer buildWith( APIEndpoint ep, ShortnameService sns ) {
-		// TODO Auto-generated method stub
 		return new Demo_HTML_Renderer( ep, sns );
 	}
 
 	@Override public RendererFactory withRoot(Resource uri) {
-		// TODO Auto-generated method stub
 		return this;
 	}
 
 	@Override public RendererFactory withMediaType(MediaType mt) {
-		// TODO Auto-generated method stub
 		return this;
 	}
 
