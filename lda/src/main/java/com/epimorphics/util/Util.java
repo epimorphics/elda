95893cf4048495803d70d45eb7740fc8f7175e8a
Chris Dollin <ehog.hedge@gmail.com>
2011-12-01 12:11:28 +0000

    Removed a bunch of unused utility functions.

--- lda/src/main/java/com/epimorphics/util/Util.java
+++ lda/src/main/java/com/epimorphics/util/Util.java
@@ -11,19 +11,12 @@ package com.epimorphics.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.List;
 
 import com.epimorphics.lda.exceptions.EldaException;
-import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.shared.WrappedException;
 import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class Util
     {   
@@ -50,52 +43,6 @@ public class Util
             .replace( "{$body}", body )
             ;
         }
-
-    public static String urlEncode( String s )
-        {
-        try { return URLEncoder.encode( s, "UTF-8" ); }
-        catch (UnsupportedEncodingException e) { throw new WrappedException( e ); }
-        }
-    
-    public static String urlDecode( String s )
-        {
-        try { return URLDecoder.decode( s, "UTF-8" ); }
-        catch (UnsupportedEncodingException e) { throw new WrappedException( e ); }
-        }
-    
-    public static String shortForm( Resource p )
-        {
-        try { return p.getModel().shortForm( p.getURI() ); }
-        catch (NullPointerException e) { return p.getURI(); } // HACK -- somehow null values are getting into the pm
-        }
-
-    private static String shortForm( RDFNode r, Node n )
-        {
-        try { return ((Resource) r).getModel().shortForm( n.getURI() ); }
-        catch (NullPointerException e) { return n.getURI(); } // HACK -- somehow null values are getting into the pm
-        }
-
-    public static String niceName( Resource p )
-        {
-        List<Statement> labels = p.listProperties( RDFS.label ).toList();
-        return labels.isEmpty() ? Util.shortForm( p ) : labels.get( 0 ).getString();
-        }
-
-    public static String urlEncodeNode( RDFNode v )
-        { return urlEncode( encodeNode( v ) ); }
-
-    private static String encodeNode( RDFNode r )
-        {
-        Node n = r.asNode();
-        if (n.isURI()) return shortForm( r, n );
-        if (n.isLiteral()) return encodeLiteral( n );
-        throw new UnsupportedOperationException( "not supported: " + r );
-        }
-
-    private static String encodeLiteral( Node lit )
-        {
-        return "\"" + lit.getLiteralLexicalForm() + "\"";
-        }       
 
     /**
         Answer the URI with the given spelling. If there's a syntax error,
