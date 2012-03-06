89d854639e5ee7ad5c22bae1f4aa52f8c3252b84
Chris Dollin <ehog.hedge@gmail.com>
2012-03-06 10:25:41 +0000

    RouterRestlet may produce any type.

--- lda/src/main/java/com/epimorphics/lda/restlets/RouterRestlet.java
+++ lda/src/main/java/com/epimorphics/lda/restlets/RouterRestlet.java
@@ -98,7 +98,7 @@ import com.hp.hpl.jena.shared.WrappedException;
         return match;
     }
     
-    @GET @Produces( { "text/plain", "application/rdf+xml", "application/json", "text/turtle", "text/html", "text/xml" })
+    @GET @Produces( { "text/plain", "application/rdf+xml", "application/json", "text/turtle", "text/html", "text/xml", "*/*" })
     public Response requestHandler(
             @PathParam("path") String pathstub,
             @Context HttpHeaders headers, 
