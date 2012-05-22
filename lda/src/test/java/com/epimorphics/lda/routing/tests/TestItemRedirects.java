4e2dafd4057287677ab2dba29daf042b55419d3c
Chris Dollin <ehog.hedge@gmail.com>
2012-05-22 14:09:19 +0100

    Extending test cases (a bit).

--- lda/src/test/java/com/epimorphics/lda/routing/tests/TestItemRedirects.java
+++ lda/src/test/java/com/epimorphics/lda/routing/tests/TestItemRedirects.java
@@ -10,7 +10,10 @@ package com.epimorphics.lda.routing.tests;
 import static org.junit.Assert.*;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.epimorphics.jsonrdf.utils.ModelIOUtils;
@@ -23,6 +26,7 @@ import com.epimorphics.lda.specs.APISpec;
 import com.epimorphics.lda.tests_support.LoadsNothing;
 import com.epimorphics.util.URIUtils;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.util.FileManager;
 
@@ -50,7 +54,7 @@ public class TestItemRedirects {
 	    Test that a (Default) Router will find the correct URI template associated
 	    with an ItemTemplate that matches a given path.
 	*/
-	@Test public void testFindsURITemplateForItem() {
+	@Test @Ignore public void testFindsURITemplateForItem() {
 		URI req = URIUtils.newURI( "" );
 		Resource root = specModel.createResource( specModel.expandPrefix( ":root" ) );
 		APISpec spec = new APISpec( FileManager.get(), root, LoadsNothing.instance );
@@ -60,11 +64,85 @@ public class TestItemRedirects {
 		assertEquals( apiBase + "other/2001", r.findItemURIPath( req, "/not/this/2001" ) );
 	}
 
-	private void loadRouter( Router r, APISpec spec ) {
+	private static void loadRouter( Router r, APISpec spec ) {
 		for (APIEndpointSpec eps: spec.getEndpoints()) {
 			APIEndpoint ep = new APIEndpointImpl( eps );
 			r.register( ep.getURITemplate(), ep );
 		}	
 	}
+	
+	String [][] testCases = new String[][] {
+			{ "1", "base:uri/{A}", "thing/{A}", "/A", "base:uri/", "thing/A" },
+	};
+	
+	@Test public void testing() {
+		StringBuilder errors = new StringBuilder();
+		for (String [] tc: testCases) {
+			try {
+				testInverseLookup( tc[1], tc[2], tc[3], tc[4], tc[5] );
+			} catch (AssertionError e) {
+				errors.append( "\n" ).append( tc[0] ).append( ": " ).append( e.getMessage() );
+			}
+		}
+		if (errors.length() > 0) {
+			fail( errors.toString() );
+		}
+	}
+
+	private void testInverseLookup
+		( String itemTemplate, String uriTemplate, String path, String baseURI, String expected ) {
+		Setup s = Setup.prepare( baseURI, itemTemplate, uriTemplate );
+		String answer = s.invert( path, baseURI );
+		assertEquals( expected, answer );		
+	}
+	
+	static class Setup {
+		
+		final Router r = new DefaultRouter();
+		final APISpec spec;
+		
+		Setup( APISpec spec ) {
+			this.spec = spec;			
+		}
+		
+		static final String configTemplate =
+			":root a api:API"
+			+ "\n  ; api:sparqlEndpoint <unused:endpoint>"
+			+ "\n  ; api:base '{{BASE}}'"
+			+ "\n  ; api:endpoint :A"
+			+ "\n."
+			+ "\n:A a api:ItemEndpoint"
+			+ "\n  ; api:uriTemplate '{{UT}}'"
+			+ "\n  ; api:itemTemplate '{{IT}}'"
+			+ "\n."
+			;
+		
+		static Setup prepare( String apiBase, String itemTemplate, String uriTemplate ) {
+			String config = expand( configTemplate, apiBase, itemTemplate, uriTemplate );
+			Model m = ModelIOUtils.modelFromTurtle( config );
+						
+			Resource root = m.createResource( m.expandPrefix( ":root" ) );
+			APISpec spec = new APISpec( FileManager.get(), root, LoadsNothing.instance );
+			
+			Setup result = new Setup( spec );
+			loadRouter( result.r, spec ); 
+			return result;
+		}
+		
+		private static String expand(String template, String apiBase, String itemTemplate, String uriTemplate) {
+			return template
+				.replace( "{{IT}}", itemTemplate )
+				.replace( "{{UT}}", uriTemplate )
+				.replace( "{{BASE}}", apiBase )
+				;
+		}
+
+		String invert( String path, String baseURI ) {
+			return r.findItemURIPath( URIUtils.newURI( baseURI ), path );
+		}
+		
+	}
+	
+	
 
 }
