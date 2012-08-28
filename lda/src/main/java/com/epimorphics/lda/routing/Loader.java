ff30362ef7bb7da25e5c1150f818d5ee74ece78f
Chris Dollin <ehog.hedge@gmail.com>
2012-08-28 15:01:27 +0100

    Loader allows spaces around commas in multiple configs.

--- lda/src/main/java/com/epimorphics/lda/routing/Loader.java
+++ lda/src/main/java/com/epimorphics/lda/routing/Loader.java
@@ -171,7 +171,7 @@ public class Loader extends HttpServlet {
     }
 
     private String[] safeSplit(String s) {
-        return s == null || s.equals("") ? new String[] {} : s.split(",");
+        return s == null || s.equals("") ? new String[] {} : s.split(" *, *");
     }
 
     public static final String DATASTORE_KEY = "com.epimorphics.api.dataStoreDirectory";
