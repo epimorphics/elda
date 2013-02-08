ef284d00e79f072835892e8168a325f8a69c3cd4
Chris Dollin <ehog.hedge@gmail.com>
2013-02-08 16:15:56 +0000

    revise pseudo-tag

--- lda/src/main/java/com/epimorphics/lda/vocabularies/ELDA.java
+++ lda/src/main/java/com/epimorphics/lda/vocabularies/ELDA.java
@@ -27,7 +27,7 @@ public class ELDA
 	{
 	static final String version = Version.string;
 	
-	static final String tag = "";
+	static final String tag = " [A]";
 	
 	static final private Model m = ModelFactory.createDefaultModel();
 	
