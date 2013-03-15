2b5179647188ec5d14d8ee904a3e0cbd6ce0dd90
Chris Dollin <ehog.hedge@gmail.com>
2013-03-15 12:14:31 +0000

    preserve some safe changes

--- lda/src/test/java/temp/TestContextFromShortnameService.java
+++ lda/src/test/java/temp/TestContextFromShortnameService.java
@@ -2,13 +2,11 @@ package temp;
 
 import static org.junit.Assert.*;
 
-import java.util.Map;
 
 import org.junit.Test;
 
 import com.epimorphics.jsonrdf.Context;
-import com.epimorphics.jsonrdf.ContextPropertyInfo;
-import com.epimorphics.lda.shortnames.NameMap;
+import com.epimorphics.lda.shortnames.ContextMaker;
 import com.epimorphics.lda.shortnames.ShortnameService;
 import com.epimorphics.lda.shortnames.StandardShortnameService;
 
@@ -18,25 +16,9 @@ public class TestContextFromShortnameService {
 		
 		ShortnameService sns = new StandardShortnameService();		
 		Context fromSNS = sns.asContext();
-		Context byConstruction = contextFrom( sns );
+		Context byConstruction = ContextMaker.contextFrom( sns );
 		
-//		System.err.println( fromSNS.diff( byConstruction ) );
-//		assertEquals( fromSNS, byConstruction );
-	}
-
-	private Context contextFrom(ShortnameService sns) {
-		Context result = new Context();
-		NameMap nm = sns.nameMap();
-	//
-		Map<String, String> uriToNames = nm.stage2().result();
-		for (Map.Entry<String, String> e: uriToNames.entrySet()) {
-			result.recordPreferredName( e.getValue(), e.getKey() );
-		}
-	//
-		Map<String, ContextPropertyInfo> im = nm.getInfoMap();
-		for (Map.Entry<String, ContextPropertyInfo> e: im.entrySet()) {
-			result.setProperty( e.getKey(), e.getValue().clone() );
-		}
-		return result;
+		System.err.println( fromSNS.diff( byConstruction ) );
+		assertEquals( fromSNS, byConstruction );
 	}
 }
