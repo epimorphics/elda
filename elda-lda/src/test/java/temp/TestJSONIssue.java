e8bf5fe343425f82ed627014d13877da0daefda7
Chris Dollin <ehog.hedge@googlemail.com>
2014-07-16 11:47:22 +0100

    All java files that did not contain the string 'copyright' have had a copyright header prefixed onto them.

--- elda-lda/src/test/java/temp/TestJSONIssue.java
+++ elda-lda/src/test/java/temp/TestJSONIssue.java
@@ -1,3 +1,11 @@
+/*
+    See lda-top/LICENCE (or https://raw.github.com/epimorphics/elda/master/LICENCE)
+    for the licence for this software.
+    
+    (c) Copyright 2014 Epimorphics Limited
+    $Id$
+*/
+
 package temp;
 
 import java.io.*;
