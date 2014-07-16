0e5eac51ceaf75001d5de1df266e0ace77137d0e
Chris Dollin <ehog.hedge@googlemail.com>
2014-07-16 15:56:49 +0100

    Added some explanatory commentary on /meta.

--- elda-lda/src/main/java/com/epimorphics/lda/core/APIEndpointImpl.java
+++ elda-lda/src/main/java/com/epimorphics/lda/core/APIEndpointImpl.java
@@ -155,12 +155,18 @@ public class APIEndpointImpl implements APIEndpoint {
     	return spec.isListEndpoint();
     }
 
+    // template -- the unexpanded uri template for this endpoint
+    // expanded -- that same template but with all possible variables expanded
 	private String createDefinitionURI( URI ru, Resource uriForSpec, String template, String expanded ) {
+		// the pseudo-template converts {name} to _name so that it's a legal
+		// URI component for feeding back into a request URI.
     	String pseudoTemplate = template.replaceAll( "\\{([A-Za-z0-9]+)\\}", "_$1" );
+    //
        	if (pseudoTemplate.startsWith("http:")) {
     		// Avoid special case from the TestAPI uriTemplates, qv.
     		return pseudoTemplate + "/meta";
     	}
+    //
 		String other = ru.toString().replace( expanded, "/meta" + pseudoTemplate );
 //		System.err.println( ">> createDefinitionURI" );
 //		System.err.println( ">> ru: " + ru );
