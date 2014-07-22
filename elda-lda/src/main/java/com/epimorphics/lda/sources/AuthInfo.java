153eb81fa3e501da7435d339a536c28569f221f1
Chris Dollin <ehog.hedge@googlemail.com>
2014-07-22 10:23:47 +0100

    Revisions to basic auth prototype.

--- elda-lda/src/main/java/com/epimorphics/lda/sources/AuthInfo.java
+++ elda-lda/src/main/java/com/epimorphics/lda/sources/AuthInfo.java
@@ -8,20 +8,25 @@
 
 package com.epimorphics.lda.sources;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // EXPLORATORY.
 public class AuthInfo {
 	
-	Map<String, String> map = new HashMap<String, String>();
+//	Map<String, String> map = new HashMap<String, String>();
 
+	final Properties map;
+	
+	public AuthInfo(Properties p) {
+		this.map = p;
+	}	
+	
 	public void put(String k, String v) {
 		map.put(k, v);
 	}
 
 	public String get(String key) {
-		return map.get(key);
+		return (String) map.get(key);
 	}
 	
 	@Override public String toString() {
