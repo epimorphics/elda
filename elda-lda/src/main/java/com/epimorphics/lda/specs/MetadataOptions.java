513de2dd03a586b587045bfda18df37c017f4680
Chris Dollin <ehog.hedge@gmail.com>
2014-07-25 15:18:21 +0100

    Removed uncalled set() and extract() methods as easy clean-up.

--- elda-lda/src/main/java/com/epimorphics/lda/specs/MetadataOptions.java
+++ elda-lda/src/main/java/com/epimorphics/lda/specs/MetadataOptions.java
@@ -13,39 +13,16 @@ import java.util.*;
 import com.epimorphics.lda.vocabularies.ELDA_API;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.shared.BrokenException;
 
 public class MetadataOptions {
-    
+
 	/**
-	    If there are any statements (R EXTRAS.metadataOptions S) then clear
-	    <code>metadataOptions</code> and then add all the comma-separated
-	    components of all the S's lowercased.
+	    Returns an array of strings S which are the comma-separated
+	    elements of all the elda:metadataOptions values of R, forced
+	    to lower-case.
 	*/
-    public static void extract( Set<String> metadataOptions, Resource R ) {
-    	List<Statement> options = R.listProperties( ELDA_API.metadataOptions ).toList();
-    	if (options.size() > 0) {
-    		metadataOptions.clear();
-    		for (Statement os: options)
-    			set( metadataOptions, os.getString() );
-    	}
-    }
-
-    /**
-        Add to <code>metadataOptions</code> lower-case versions of the comma-
-        separated elements of <code>mdo</code>.
-    */
-	public static void set( Set<String> metadataOptions, String mdo ) {
-		if (mdo.length() > 0)
-			for (String option: mdo.split( " *, *" ))
-				metadataOptions.add( option.toLowerCase() );
-	}
-
 	public static String[] get( Resource R ) {
 		List<String> result = new ArrayList<String>();
-		
-		if (R == null) throw new BrokenException(">> OOPSY-DAISY.");
-		
 		List<Statement> options = R.listProperties( ELDA_API.metadataOptions ).toList();
     	if (options.size() > 0) 
     		for (Statement os: options)
