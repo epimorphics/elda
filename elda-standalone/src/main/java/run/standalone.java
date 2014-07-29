04bb727d516ab8511af9e2d0c37e65c918ec3589
Ian Dickinson <ian@epimorphics.com>
2014-07-29 18:05:18 +0100

    WIP: still trying to get a workable development process for maven/jetty/elda/eclipse

--- elda-standalone/src/main/java/run/standalone.java
+++ elda-standalone/src/main/java/run/standalone.java
@@ -1,30 +1,33 @@
 /*
     See lda-top/LICENCE (or https://raw.github.com/epimorphics/elda/master/LICENCE)
     for the licence for this software.
-    
+
     (c) Copyright 2014 Epimorphics Limited
     $Id$
 */
 
 package run;
 
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.servlet.ServletHolder;
-import org.mortbay.jetty.webapp.WebAppContext;
+//import org.mortbay.jetty.Server;
+//import org.mortbay.jetty.servlet.ServletHolder;
+//import org.mortbay.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class standalone {
 
-	public static void main( String [] args ) throws Exception {  		
-	    Server server = new Server(8080);
-	//	    
-	    WebAppContext webapp = new WebAppContext( "src/main/webapp/", "/standalone" );
-	    webapp.addServlet(new ServletHolder( new ServletContainer( new PackagesResourceConfig("com.epimorphics.lda.restlets") ) ), "/");
+    public static void main( String [] args ) throws Exception {
+        Server server = new Server(8080);
+    //
+        WebAppContext webapp = new WebAppContext( "src/main/webapp/", "/standalone" );
+        webapp.addServlet(new ServletHolder( new ServletContainer( new PackagesResourceConfig("com.epimorphics.lda.restlets") ) ), "/");
         server.setHandler(webapp);
     //
-	    server.start();
-	    server.join();
-	} 
+        server.start();
+        server.join();
+    }
 }
