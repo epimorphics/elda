924424d918b1b8bd044d9bb3e3f0778f3d4d2829
Ian Dickinson <i.j.dickinson@gmail.com>
2014-08-15 17:52:23 +0100

    First cut at general results map

--- elda-assets/src/main/webapp/velocity/js/maps.js
+++ elda-assets/src/main/webapp/velocity/js/maps.js
@@ -3,9 +3,10 @@ var EldaMaps = function() {
   /** The correct projection for the UK - other locations may require different projections */
   var projection = new OpenLayers.Projection( "EPSG:4326" );
 
+  /** Add a small map, suitable for displaying per item */
   var addSmallMap = function( divID, lon, lat, markerImage ) {
-    var map = createMap( divID );
-    addControlLayers( map );
+    var map = createMap( divID, smallMapControls() );
+    addOSMLayer( map );
     centreMap( lon, lat, map );
 
     var markersLayer = new OpenLayers.Layer.Markers( "Markers" );
@@ -13,19 +14,52 @@ var EldaMaps = function() {
     addMarker( markersLayer, lon, lat, markerImage, map );
   };
 
-  /** @return A new map object for the given div */
-  var createMap = function( divID ) {
-    var controls = controlsList();
-    return new OpenLayers.Map( divID, {controls: controls} );
+  /** Add a large map, for showing a group of items */
+  var addLargeMap = function( divId, locations, markerImage ) {
+    var map = createMap( divId, largeMapControls() );
+
+    var markersLayer = new OpenLayers.Layer.Markers("Markers");
+    map.addLayer( markersLayer );
+    addOSMLayer( map );
+
+    var bounds = new OpenLayers.Bounds();
+
+    for( var i = 0; i < locations.length; i++) {
+      var loc = locations[i];
+      var point = longLatPoint( loc.lon, loc.lat, map );
+
+      bounds.extend( point );
+
+      addMarker( markersLayer, loc.lon, loc.lat, markerImage, map );
+    }
+
+    var zoom = map.getZoomForExtent(bounds);
+    map.setCenter( bounds.getCenterLonLat(), zoom < 14 ? zoom : 14 );
   };
 
-  /** @return The controls we want to display on the map */
-  var controlsList = function() {
+  /** @return A new map object for the given div */
+  var createMap = function( divID, controls ) {
+     return new OpenLayers.Map( divID, {controls: controls} );
+  };
+
+  /** @return The controls we want to display on the small map */
+  var smallMapControls = function() {
     return [new OpenLayers.Control.ArgParser()];
   };
 
+  /** @return The list of controls for the large map */
+  var largeMapControls = function() {
+    return [
+      new OpenLayers.Control.Navigation(),
+      new OpenLayers.Control.KeyboardDefaults(),
+      new OpenLayers.Control.ZoomBox(),
+      new OpenLayers.Control.ZoomPanel(),
+      new OpenLayers.Control.Attribution()
+    ];
+  };
+
   /** Add layers to the given map */
-  var addControlLayers = function( map ) {
+  var addOSMLayer = function( map ) {
     map.addLayer( new OpenLayers.Layer.OSM.Mapnik() );
   };
 
@@ -47,15 +81,22 @@ var EldaMaps = function() {
 
   /** Add a marker for the given location */
   var addMarker = function( markersLayer, lon, lat, markerImage, map ) {
-    var size = new OpenLayers.Size( markerImage.w, markerImage.h );
-    var offset = new OpenLayers.Pixel( -(size.w/2), -size.h );
-    var icon = new OpenLayers.Icon( markerImage.image, size, offset );
-    var marker = new OpenLayers.Marker( longLatPoint( lon, lat, map ), icon );
+    if (markerImage.icon) {
+      var marker = new OpenLayers.Marker( longLatPoint( lon, lat, map ), markerImage.icon.clone() );
+    }
+    else {
+      var size = new OpenLayers.Size( markerImage.w, markerImage.h );
+      var offset = new OpenLayers.Pixel( -(size.w/2), -size.h );
+      var icon = new OpenLayers.Icon( markerImage.image, size, offset );
+      var marker = new OpenLayers.Marker( longLatPoint( lon, lat, map ), icon );
+      markerImage.icon = icon;
+    }
 
     markersLayer.addMarker(marker);
   };
 
   return {
+    addLargeMap: addLargeMap,
     addSmallMap: addSmallMap
   };
 }();
