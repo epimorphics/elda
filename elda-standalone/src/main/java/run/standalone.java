7978375f17d9993b1e1c9ba80a60d3a550cd0027
Chris Dollin <ehog.hedge@googlemail.com>
2014-11-06 11:13:59 +0000

    run-tomcat-from-main now works, but it's because of some hidden change that we can't see. We defined a new triial project, copied standalone into it -- it still worked -- then copied it back carefully, now the original works. Baffling at present.

--- elda-standalone/src/main/java/run/standalone.java
+++ elda-standalone/src/main/java/run/standalone.java
@@ -1,35 +1,30 @@
-/*
-    See lda-top/LICENCE (or https://raw.github.com/epimorphics/elda/master/LICENCE)
-    for the licence for this software.
-    
-    (c) Copyright 2014 Epimorphics Limited
-    $Id$
-*/
-
 package run;
 
+import java.io.File;
+
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.Context;
 
-import com.sun.jersey.api.core.PackagesResourceConfig;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
-
-public class standalone {
-
+public class standalone {	
+	
 	static final String contextPath = "/standalone";
 
-	public static void main( String [] args ) throws Exception {  		
+	public static void main( String [] args ) throws Exception {  
+		
 	    Tomcat server = new Tomcat(); 
 	    server.setPort(8080);
 	//	    
 	    server.setBaseDir(".");
-	    PackagesResourceConfig p = new PackagesResourceConfig("com.epimorphics.lda.restlets");
-	    ServletContainer c = new ServletContainer(p);
-	//
-	    Context cx = server.addContext(contextPath, "GOOPSTY");
-	    Tomcat.addServlet(cx, contextPath, c);
-    //
+	
+	    server.addWebapp(contextPath,  new File("src/main/webapp").getAbsolutePath());
+	    
+	//    PackagesResourceConfig p = new PackagesResourceConfig("com.epimorphics.lda.restlets");
+	//    ServletContainer c = new ServletContainer(p);
+	////
+	//    Context cx = server.addContext(contextPath, "GOOPSTY");
+	//    Tomcat.addServlet(cx, contextPath, c);
+	////
 	    server.start();
 	    server.getServer().await();
 	} 
+
 }
