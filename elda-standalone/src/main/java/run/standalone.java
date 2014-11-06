b66dd220f68a867b15c595ab3a4e68984577827d
Chris Dollin <ehog.hedge@googlemail.com>
2014-11-06 11:29:33 +0000

    Pruned dead commented-out code and newlines.

--- elda-standalone/src/main/java/run/standalone.java
+++ elda-standalone/src/main/java/run/standalone.java
@@ -9,20 +9,12 @@ public class standalone {
 	static final String contextPath = "/standalone";
 
 	public static void main( String [] args ) throws Exception {  
-		
 	    Tomcat server = new Tomcat(); 
-	    server.setPort(8080);
-	//	    
+	    server.setPort(8080);  
 	    server.setBaseDir(".");
-	
+	//
 	    server.addWebapp(contextPath,  new File("src/main/webapp").getAbsolutePath());
-	    
-	//    PackagesResourceConfig p = new PackagesResourceConfig("com.epimorphics.lda.restlets");
-	//    ServletContainer c = new ServletContainer(p);
-	////
-	//    Context cx = server.addContext(contextPath, "GOOPSTY");
-	//    Tomcat.addServlet(cx, contextPath, c);
-	////
+	//
 	    server.start();
 	    server.getServer().await();
 	} 
