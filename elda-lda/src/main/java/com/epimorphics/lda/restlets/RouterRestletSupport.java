1cc2bc448ade6741b9cabda06d3758eb8bf16879
Chris Dollin <ehog.hedge@googlemail.com>
2014-12-08 14:33:20 +0000

    Simplified system tests to make progress.

--- elda-lda/src/main/java/com/epimorphics/lda/restlets/RouterRestletSupport.java
+++ elda-lda/src/main/java/com/epimorphics/lda/restlets/RouterRestletSupport.java
@@ -89,8 +89,9 @@ public class RouterRestletSupport {
 				pfs.add( new PrefixAndFilename( prefixName, specName ) );
 			} else {
 				String fullPath = specName.startsWith("/") ? specName : baseFilePath + specName;
+				log.debug("spec file pattern is " + fullPath);
 				List<File> files = new Glob().filesMatching( fullPath );
-				// log.info( "full path " + fullPath + " matches " + files.size() + " files." );
+				log.debug( "full path " + fullPath + " matches " + files.size() + " files." );
 				for (File f: files) {
 					String expandedPrefix = ServletUtils.containsStar(prefixName) ? ServletUtils.nameToPrefix(prefixName, specName, f.getName()) : prefixName;
 					pfs.add( new PrefixAndFilename( expandedPrefix, f.getAbsolutePath() ) );
--- elda-testing-webapp/src/test/java/com/epimorphics/lda/testing/utils/TomcatTestBase.java
+++ elda-testing-webapp/src/test/java/com/epimorphics/lda/testing/utils/TomcatTestBase.java
@@ -57,6 +57,7 @@ public abstract class TomcatTestBase {
             System.exit(1);
         }
 
+        System.err.println(">> addWebapp(" + contextPath + ", " + rootF.getAbsolutePath() +")");
         tomcat.addWebapp(contextPath,  rootF.getAbsolutePath());
         tomcat.start();
 
