7c26dee972cad79847965dc0abdaccf13d9b1d9a
Chris Dollin <ehog.hedge@googlemail.com>
2014-12-18 08:40:34 +0000

    Revised several parameter bad requests to use EldaException.BadRequest.

--- elda-lda/src/main/java/com/epimorphics/lda/query/ContextQueryUpdater.java
+++ elda-lda/src/main/java/com/epimorphics/lda/query/ContextQueryUpdater.java
@@ -159,7 +159,7 @@ public class ContextQueryUpdater implements ViewSetter {
             aq.setSubjectAsItemEndpoint(val);
         } else if (p.equals( QueryParameter.callback )) {
             if (!QueryParameter.callbackPattern.matcher( val ).matches())
-                throw new EldaException( "illegal callback name", val, EldaException.BAD_REQUEST );
+                EldaException.BadRequest("illegal callback name " + val);
         } else if (p.equals( QueryParameter.NEAR_LAT)) {
             geo.setNearLat( val );
         } else if (p.equals( QueryParameter.NEAR_LONG )) {
@@ -176,9 +176,9 @@ public class ContextQueryUpdater implements ViewSetter {
 
             Boolean count = getBoolean(val);
             if (count == null)
-                throw new EldaException("illegal boolean (should be 'yes' or 'no') for _count.", val, EldaException.BAD_REQUEST);
+                EldaException.BadRequest("illegal boolean (should be 'yes' or 'no') for _count: " + val);
             else if (!aq.setTotalCountRequested( count ))
-                throw new EldaException("this endpoint does not allow _count to be altered.", val, EldaException.BAD_REQUEST);
+                EldaException.BadRequest("this endpoint does not allow _count to be altered.");
 
         } else if (!allowedReserved( p )){
             EldaException.BadRequest( "unrecognised reserved parameter: " + p );
