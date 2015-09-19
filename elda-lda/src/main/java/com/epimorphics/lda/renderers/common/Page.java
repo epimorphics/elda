63c3cf6697ebd10c631c747e0b16a15e493c43d8
Ian Dickinson <i.j.dickinson@gmail.com>
2015-09-19 10:51:45 +0100

    Fix for issue #153: when the results count is zero, show an appropriate count summary

--- elda-lda/src/main/java/com/epimorphics/lda/renderers/common/Page.java
+++ elda-lda/src/main/java/com/epimorphics/lda/renderers/common/Page.java
@@ -146,8 +146,8 @@ public class Page extends CommonNodeWrapper
             int perPage = itemsPerPage();
             int from = startIndex();
             
-            if (perPage > 0 && from >= 0) {
-                int to = from + perPage;
+            if (perPage > 0 && from >= 0 && total > 0) {
+                int to = from + perPage - 1;
                 if (to > total) {
                     to = total;
                 }
@@ -155,7 +155,8 @@ public class Page extends CommonNodeWrapper
                 s = String.format( "Showing items %d to %d of %d", from, to, total );
             }
             else {
-                s = String.format( "%d total results", total );
+                String plural = (total == 1) ? "" : "s";
+                s = String.format( "%d result%s", total, plural );
             }
         }
         
