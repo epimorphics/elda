8fa5532be3b0a5a54a34ef98c5fb8930c3a857c3
Chris Dollin <ehog.hedge@googlemail.com>
2016-10-26 12:02:37 +0100

    version tag

--- elda-lda/src/main/java/com/epimorphics/lda/Version.java
+++ elda-lda/src/main/java/com/epimorphics/lda/Version.java
@@ -17,5 +17,5 @@ package com.epimorphics.lda;
  *
  */
 public interface Version {
-	public final String string = "1.3.19-SNAPSHOTreordering";
+	public final String string = "1.3.19-SNAPSHOT-reordered";
 }
