22acc43851cf494e2a340ffb7f33782f5c54e536
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-07-20 17:12:18 +0100

    Proposed fix for #188 by replacing the current static CSS and JS files for qonsole with copies from the latest build of the qonsole repo. This is not a very satisfactory long-term solution, since it's hard to tell what versions of files are deployed in Elda, and all of the files are served separately rather than being webpacked into one artefact. Long-term aim is to add package.json support here, and use npm or yarn to load the dependencies. For the time being, we just copy the files into this repo.

--- elda-assets/src/main/webapp/velocity/js/remote-sparql-service.js
+++ elda-assets/src/main/webapp/velocity/js/remote-sparql-service.js
@@ -1,80 +1,98 @@
 /** A simple interface to a remote SPARQL service */
-define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
-  /* jshint strict: true, undef:true */
-  "use strict";
 
-  /** This class provides a duck-typed API for accessing the content of a value
-   * returned from a SPARQL service. The key methods are:
-   *
-   * asText - returns the result as a suitably formatted text string
-   * asJSON - returns the result as a suitably formatted JSON string
-   * asXML - returns the result as a suitable formatted XML string
-   * asTable - returns the result as an array of objects, with one key per object per query variable
-   */
-  var RemoteSparqlServiceResult = function( val, format ) {
+/** This class provides a duck-typed API for accessing the content of a value
+ * returned from a SPARQL service. The key methods are:
+ *
+ * asText - returns the result as a suitably formatted text string
+ * asJSON - returns the result as a suitably formatted JSON string
+ * asXML - returns the result as a suitable formatted XML string
+ * asTable - returns the result as an array of objects, with one key per object per query variable
+ */
+
+define( [
+  'lodash',
+  'sprintf'
+],
+function (
+  _,
+  sprintf
+) {
+  'use strict';
+
+  var RemoteSparqlServiceResult = function ( val, format ) {
     this._val = val;
     this._format = format;
   };
 
   _.extend( RemoteSparqlServiceResult.prototype, {
-    asText: function() {
+    asText: function () {
       var data = this._val;
+      var isTabular = data.match( /^----/ );
+      var count = isTabular ? (data.split('\n').length - 5) : 1;
+
       return {
-        count: data.split('\n').length - 5,
+        count: count,
         data: data,
-        mime: "text/plain"
+        mime: 'text/plain'
       };
     },
 
-    asJSON: function() {
-      var json, data = this._val;
+    asJSON: function () {
+      var json = null;
+      var data = this._val;
 
       if (_.isString( data )) {
         json = data;
         data = JSON.parse(data);
-      }
-      else {
+      }      else {
         // en bas le Internet Explorer
         json = JSON.stringify( data, null, 2 );
       }
 
+      var singleResult = !data.results;
+
       return {
-        count: data.results.bindings.length,
+        count: singleResult ? 1 : data.results.bindings.length,
         data: json,
-        mime: "application/json"
+        mime: 'application/json'
       };
     },
 
-    asXML: function() {
-      var count, xml, data = this._val;
+    asXML: function () {
+      var xml = null;
+      var data = this._val;
 
       if (_.isString( data )) {
         xml = data;
         data = $.parseXML( data );
-      }
-      else {
+      }      else {
         xml = this.xmlToString( data );
       }
 
+      var $data = $( data );
+      var singleResult = $data.find( 'results' ).length === 0;
+
       return {
-        count: $( data ).find("results").children().length,
+        count: singleResult ? 1 : $data.find('results').children().length,
         data: xml,
-        mime: "application/xml"
+        mime: 'application/xml'
       };
     },
 
-    asTable: function( config ) {
+    asTable: function ( config ) {
       /* Shorten a URI to qname form, if possible */
-      var toQName = function( prefixes, uri ) {
-        var result = uri, qname, u = uri;
+      var toQName = function ( prefixes, uri ) {
+        var result = uri;
+        var qname;
+        var u = uri;
 
         if (u.substring( 0, 1 ) === '<') {
           u = u.substring( 1, u.length - 1 );
         }
 
-        $.each( prefixes, function( prefix, prefURI ) {
+        $.each( prefixes, function ( prefix, prefURI ) {
           if (u.indexOf( prefURI ) === 0) {
-            qname = sprintf( "%s:%s", prefix, u.substring( prefURI.length ) );
+            qname = sprintf.sprintf( '%s:%s', prefix, u.substring( prefURI.length ) );
 
             if (qname.length < result.length) {
               result = qname;
@@ -84,22 +102,24 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
         return result;
       };
 
-      /** Format a value for display in the table view */
-      var dataTableValue = function( v ) {
-        var f, parts;
+      /**
+       * Format a value for display in the table view
+       * @param  {any} v Value to format
+       * @return {string} Value formatted for datatables
+      */
+      var dataTableValue = function ( v ) {
+        var f;
+        var parts;
 
         if (_.isNumber( v )) {
           f = parseFloat( v );
-        }
-        else if (v.match( /\^\^/ )) {
+        }        else if (v.match( /\^\^/ )) {
           parts = v.match( /^"*([^\\^\\""]*)"*\^\^<*(.*)>*$/m );
-          f = sprintf( "<span title='Type: %s'>%s</span>", parts[2], parts[1]);
-        }
-        else if (v.match( /@/ )) {
+          f = sprintf.sprintf( "<span title='Type: %s'>%s</span>", parts[2], parts[1]);
+        }        else if (v.match( /^"(.*)"@([^@]*)/ )) {
           parts = v.match( /^"(.*)"@([^@]*)/ );
-          f = sprintf( "<span title='Language: %s'>%s</span>", parts[2], parts[1] );
-        }
-        else {
+          f = sprintf.sprintf( "<span title='Language: %s'>%s</span>", parts[2], parts[1] );
+        }        else {
           f = toQName( config.parsedPrefixes, v );
 
           if (f.match( /^</ )) {
@@ -107,10 +127,9 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
           }
 
           if (v.match( /^<http:/ )) {
-            f = sprintf( "<a href='%s' target='_' alt='link to remote resource'>%s</a>",
+            f = sprintf.sprintf( "<a href='%s' target='_' alt='link to remote resource'>%s</a>",
                          v.slice( 1, -1 ), _.escape( f ) );
-          }
-          else {
+          }          else {
             f = _.escape(f );
           }
         }
@@ -119,14 +138,14 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
       };
 
       var data = this._val;
-      var lines = _.compact(data.split( "\n" ));
+      var lines = _.compact(data.split( '\n' ));
 
-      var columnHeaders = _.map( lines.shift().split("\t"), function( header) {
+      var columnHeaders = _.map( lines.shift().replace( /\?/g, '' ).split('\t'), function ( header) {
         return {sTitle: header};
       } );
 
-      var rows = _.map( lines, function( line ) {
-        var values = _.flatten( [line.split("\t")] );
+      var rows = _.map( lines, function ( line ) {
+        var values = _.flatten( [line.split('\t')] );
         return _.map( values, dataTableValue );
       } );
 
@@ -138,23 +157,27 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
       };
     },
 
-    asFormat: function( format, config ) {
+    asFormat: function ( format, config ) {
       switch (format) {
-        case "text":
-          return this.asText();
-        case "json":
-          return this.asJSON();
-        case "xml":
-          return this.asXML();
-        case "tsv":
-          return this.asTable( config );
+      case 'text':
+        return this.asText();
+      case 'json':
+        return this.asJSON();
+      case 'xml':
+        return this.asXML();
+      case 'tsv':
+        return this.asTable( config );
+      default:
+        return null;
       }
-
-      return null;
     },
 
-    /** Return the string representation of the given XML value, which may be a string or a DOM object */
-    xmlToString: function( xmlData ) {
+    /**
+     * Return the string representation of the given XML value, which may be a string or a DOM object
+     * @param  {object} xmlData XML data
+     * @return {string} XML formatted as a string
+     */
+    xmlToString: function ( xmlData ) {
       var xs = _.isString( xmlData ) ? xmlData : null;
 
       if (!xs && window.ActiveXObject && xmlData.xml) {
@@ -171,17 +194,16 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
 
 
   /** This class proxies a service which is behind a remote SPARQL endpoint URL */
-  var RemoteSparqlService = function() {
+  var RemoteSparqlService = function () {
   };
 
 
   _.extend( RemoteSparqlService.prototype, {
-    execute: function( query, options ) {
-
-      var ajaxDataType = function( format ) {
+    execute: function ( query, options ) {
+      var ajaxDataType = function ( format ) {
         return {
-          tsv: "html",
-          csv: "html",
+          tsv: 'html',
+          csv: 'html'
         }[format] || format;
       };
 
@@ -191,7 +213,7 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
 
       var ajaxOptions = {
         data: {query: query, output: format},
-        success: function( data ) {
+        success: function ( data ) {
           onSuccess.call( null, new RemoteSparqlServiceResult( data, format ) );
         },
         error: options.error,
@@ -203,4 +225,4 @@ define( ['jquery', 'underscore', 'sprintf'], function( $, _, sprintf ) {
   } );
 
   return RemoteSparqlService;
-});
+} );
