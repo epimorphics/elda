cb818881dfb2352d1777d82044405a56888d41a9
Chris Dollin <ehog.hedge@googlemail.com>
2018-06-18 11:14:37 +0100

    Pulled Position out of IncludeReader to reduce load on same.

---
+++ elda-lda/src/main/java/com/epimorphics/lda/configs/Position.java
@@ -0,0 +1,27 @@
+package com.epimorphics.lda.configs;
+
+/**
+	A Position specifies a 1-origin line number and the
+	pathname of the file the line number is in.
+*/
+public class Position {
+	public final String pathName;
+	public final int lineNumber;
+	
+	public Position(String pathName, int lineNumber) {
+		this.pathName = pathName;
+		this.lineNumber = lineNumber;
+	}
+	
+	@Override public String toString() {
+		return "<position " + lineNumber + " " + pathName + ">";
+	}
+	
+	@Override public boolean equals(Object other) {
+		return other instanceof Position && same((Position) other);
+	}
+
+	private boolean same(Position other) {
+		return lineNumber == other.lineNumber && pathName.equals(other.pathName);
+	}
+}
