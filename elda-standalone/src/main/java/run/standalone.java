d8dc5d467687ca6851aca2d9b1e373f0e7fcd4c1
Simon Oakes <simonoakes@epimorphics.com>
2026-06-17 22:19:47 +0100

    Tidy up standalone server impl

--- elda-standalone/src/main/java/run/standalone.java
+++ elda-standalone/src/main/java/run/standalone.java
@@ -12,31 +12,31 @@ import java.io.File;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.jena.riot.RIOT;
 
-public class standalone {	
-	
-	static final String standalone = "/standalone";
-	static final String ROOT = "/";
+public class standalone {
 
-	public static void main( String [] args ) throws Exception {  
-	    Tomcat server = new Tomcat(); 
-	    int port = 8080;
-	    String portString = System.getProperty("elda.port");
-	    if (portString != null) port = Integer.parseInt(portString);
+	public static void main( String [] args ) throws Exception {
+		RIOT.init();
+	    Tomcat server = new Tomcat();
+	    String portString = System.getProperty ("elda_port");
+		int port = portString != null ? Integer.parseInt(portString) : 8080;
+
+		String ctxPath = System.getenv("elda_context_path");
+		ctxPath = ctxPath != null ? ctxPath : "/standalone";
+
+		String basePath = System.getenv("elda_webapp_path");
+		basePath = basePath != null ? basePath : "src/main/webapp";
 
         Connector connector = new Connector();
         connector.setPort(port);
         server.getService().addConnector(connector);
-
 	    server.setBaseDir(".");
-	    
-//	    System.err.println(">> " + new File(".").getAbsolutePath());
-//	    String absolutePath = new File("target/elda-standalone").getAbsolutePath();
-	    String absolutePath = new File("src/main/webapp").getAbsolutePath();
-	    // System.err.println(">> " + absolutePath);
-		server.addWebapp(standalone, absolutePath);
-		server.addWebapp(ROOT, absolutePath);
-	//
+
+	    String absolutePath = new File(basePath).getAbsolutePath();
+		server.addWebapp(ctxPath, absolutePath);
+
+		server.getConnector();
 	    server.start();
 	    server.getServer().await();
 	} 
